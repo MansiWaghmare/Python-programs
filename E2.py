ba6f8693b97fa129fbbcd2929f660b2ef2f564dc
Mansi <121502548+MansiWaghmare@users.noreply.github.com>
2024-05-09 16:26:56 +0530

    Add files via upload

---
+++ E2.py
@@ -0,0 +1,10 @@
+# to input length, breadth of rectangle and calculate its area
+length=float(input("Enter the length of rectangle = "))
+breadth=float(input("Enter the breadth of rectangle = "))
+units=str(input("units = "))
+print("Length = ",length,units)
+print("Breadth = ",breadth,units)
+area=length*breadth
+print("Area = length * breadth")
+print("Area = ",length,units,"*",breadth,units)
+print("Area of rectangle = ",area,"sq",units)

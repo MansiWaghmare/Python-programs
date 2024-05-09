373effeecfc7996b8a3824f20c85ad850ea7e559
Mansi <121502548+MansiWaghmare@users.noreply.github.com>
2024-05-09 15:40:32 +0530

    Add files via upload
    To input three numbers and print their sum

---
+++ E1.py
@@ -0,0 +1,7 @@
+# to input three numbers and print their sum
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+num3 = int(input("Enter number 3: "))
+Sum = num1+num2+num3
+print("Three numbers are: ", num1, num2, num3)
+print("Sum = ",Sum)

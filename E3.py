36bfedbd429a68674291108202fab1f9d0b54937
Mansi <121502548+MansiWaghmare@users.noreply.github.com>
2024-05-10 19:16:46 +0530

    Add files via upload

---
+++ E3.py
@@ -0,0 +1,5 @@
+# to calculate the BMI
+weight=float(input("Enter weight in kg = "))
+height=float(input("Enter height in m = "))
+BMI=weight/(height*height)
+print("BMI = ",BMI)
---
+++ E5.py
@@ -0,0 +1,7 @@
+# area of a triangle
+
+base=float(input("Enter base of triangle = "))
+height=float(input("Enter height = "))
+area=0.5*base*height
+print("Area of triangle = ","0.5","*",base,"*",height)
+print("Area of triangle = ",area)

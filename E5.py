f66290d3cf12665e06f4d4eea3be6b5635e4adb0
Mansi <121502548+MansiWaghmare@users.noreply.github.com>
2024-05-10 19:15:56 +0530

    Delete E5.py

--- E5.py
+++
@@ -1,7 +0,0 @@
-# area of a triangle
-
-base=float(input("Enter base of triangle = "))
-height=float(input("Enter height = "))
-area=0.5*base*height
-print("Area of triangle = ","0.5","*",base,"*",height)
-print("Area of triangle = ",area)
